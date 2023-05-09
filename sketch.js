f82d104c5b0264028598e045a8331cf4c23d254c
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2023-05-09 21:08:24 +0800

    added Setting window, in progress
    added a setting window with only  1 parameter of ballcount so far

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
-let balls = [];
-
+let balls = [], ballcnt = 80;
+let settingsImg, settingsOpen = false, exitSettings;
+let ballcntSlider;
+let settingWindowSize = 350;
+function preload(){
+    settingsImg = loadImage('assets/gear.png');
+    exitSettings = loadImage('assets/close_window.png');
+}
 function setup(){
-    createCanvas(windowWidth,windowHeight);
-    for (let i = 0; i < 80; i++) {
+    createCanvas(windowWidth,windowHeight); 
+    balls = [];
+    for (let i = 0; i < ballcnt; i++) {
         balls.push(new Ball());
     }
 }
@@ -23,4 +30,36 @@ function draw(){
         balls[i].update();
         balls[i].show();
     }
+    tint(255,220);
+    image(settingsImg, windowWidth*33/35, windowHeight/75, windowHeight*7/75, windowHeight*7/75);
+    if(settingsOpen){
+        displaySettings();
+    }
+}
+function mousePressed(){
+    if(!settingsOpen && mouseX > windowWidth*33/35 && mouseX < windowWidth*33/35 + windowHeight*7/75 && mouseY > windowHeight/75 && mouseY < windowHeight/75 + windowHeight*7/75){
+        settingsOpen = true;
+        ballcntSlider = createSlider(5, 120, ballcnt,5);
+        ballcntSlider.position(windowWidth/2 - 350/2 + 50, windowHeight/2 - 350/2 + 30);
+        ballcntSlider.style('width','250px');
+    }
+    if(settingsOpen && dist(mouseX, mouseY, windowWidth/2 + settingWindowSize/2 - 45 + 50/2, windowHeight/2 - settingWindowSize/2 - 5 + 50/2) < 50/2 + 5){
+        settingsOpen = false;
+        ballcntSlider.remove();
+        setup();
+    }
+}
+function displaySettings(){
+    rectMode(CENTER);
+    fill("#E8C7C8");
+    rect(windowWidth/2, windowHeight/2, settingWindowSize, settingWindowSize,20);
+    tint(255,255);
+    image(exitSettings, windowWidth/2 + settingWindowSize/2 - 45, windowHeight/2 - settingWindowSize/2 - 5 , 50, 50);
+    textFont('Inconsolata');
+    textSize(20);
+    textAlign(CENTER);
+    fill(0);    
+    text('Number of Balls', windowWidth/2, windowHeight/2 - settingWindowSize/2 + 20);
+    ballcnt = ballcntSlider.value();
+    text(ballcnt, windowWidth/2, windowHeight/2 - settingWindowSize/2 + 60);
 }
