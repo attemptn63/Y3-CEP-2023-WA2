ed426c489dfc0e717077da2fec03c90f04fe6673
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2023-05-09 21:20:33 +0800

    added background color picker

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 let balls = [], ballcnt = 80;
 let settingsImg, settingsOpen = false, exitSettings;
-let ballcntSlider;
+let ballcntSlider,backgroundColorPicker,backgroundColor = [0,0,0];
 let settingWindowSize = 350;
 function preload(){
     settingsImg = loadImage('assets/gear.png');
     exitSettings = loadImage('assets/close_window.png');
 }
 function setup(){
-    createCanvas(windowWidth,windowHeight); 
+    createCanvas(windowWidth,windowHeight);
     balls = [];
     for (let i = 0; i < ballcnt; i++) {
         balls.push(new Ball());
@@ -15,7 +15,7 @@ function setup(){
 }
 
 function draw(){
-    background(0);
+    background(backgroundColor);
     for (let i = 0; i < balls.length; i++) {
         for (let j = i + 1; j < balls.length; j++) {
             if (balls[i].checkCollision(balls[j])) {
@@ -42,10 +42,14 @@ function mousePressed(){
         ballcntSlider = createSlider(5, 120, ballcnt,5);
         ballcntSlider.position(windowWidth/2 - 350/2 + 50, windowHeight/2 - 350/2 + 30);
         ballcntSlider.style('width','250px');
+        backgroundColorPicker = createColorPicker(color(backgroundColor[0],backgroundColor[1],backgroundColor[2]));
+        backgroundColorPicker.position(windowWidth/2 - 350/2 + 50, windowHeight/2 - 350/2 + 100);
+        backgroundColorPicker.style('width','250px');
     }
     if(settingsOpen && dist(mouseX, mouseY, windowWidth/2 + settingWindowSize/2 - 45 + 50/2, windowHeight/2 - settingWindowSize/2 - 5 + 50/2) < 50/2 + 5){
         settingsOpen = false;
         ballcntSlider.remove();
+        backgroundColorPicker.remove();
         setup();
     }
 }
@@ -55,6 +59,10 @@ function displaySettings(){
     rect(windowWidth/2, windowHeight/2, settingWindowSize, settingWindowSize,20);
     tint(255,255);
     image(exitSettings, windowWidth/2 + settingWindowSize/2 - 45, windowHeight/2 - settingWindowSize/2 - 5 , 50, 50);
+    createBallSlider();
+    createBackgroundColorPicker();
+}
+function createBallSlider(){
     textFont('Inconsolata');
     textSize(20);
     textAlign(CENTER);
@@ -62,4 +70,12 @@ function displaySettings(){
     text('Number of Balls', windowWidth/2, windowHeight/2 - settingWindowSize/2 + 20);
     ballcnt = ballcntSlider.value();
     text(ballcnt, windowWidth/2, windowHeight/2 - settingWindowSize/2 + 60);
+}
+function createBackgroundColorPicker(){
+    textFont('Inconsolata');
+    textSize(20);
+    textAlign(CENTER);
+    fill(0);    
+    text('Background Color', windowWidth/2, windowHeight/2 - settingWindowSize/2 + 90);
+    backgroundColor = backgroundColorPicker.color();
 }
